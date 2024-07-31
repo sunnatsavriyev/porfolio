68e980341158bfff7f7bef592a28fd47979903ac
sunnatsavriyev <sunnatchiksavriyev@gmail.com>
2024-07-31 18:27:56 +0500

    oxirgi commit

--- myportfolyo/urls.py
+++ myportfolyo/urls.py
@@ -21,5 +21,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('home.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+static(settings.MEDIA_) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
